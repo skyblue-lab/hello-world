30d1f9cd7c041cf3c332ef7c1b4cde676cad2791
skyblue-lab <79338320+skyblue-lab@users.noreply.github.com>
2021-03-08 13:33:33 -0700

    Revert "final commit"
    This reverts commit 0bd19d8dc063f3309b901b9cb15e202110ad6c88.

---
+++ GitDemo.java
@@ -0,0 +1,54 @@
+/*****
+ * Govind Venugopal
+ ******/
+public class GitDemo {
+    
+    private double previousNumber;
+    private double reverseNum;      // Q1ci - Added in branch GitDemo-V1
+    
+    public GitDemo() {
+        previousNumber = 1;
+    }
+    
+    /*****
+     * This function should add 'num' to 'previousNumber' and return the sum.
+    *****/
+    public double addNumber(double num) {
+        System.out.print("Add num to previousNumber");
+        previousNumber = previousNumber + num;              // Q1bi - Added in branch GitDemo-V1
+        return 0;
+    }
+    
+    /*****
+     * This function should calculate the factorial of 'num' and return the resulting value.
+     * i.e. if num = 4,
+     *  the function will return the value of (1 * 2 * 3 * 4)
+     *****/
+    public double findFactorial(int num) {      // Code swapped in GitDemo-V2
+        double newNumber = 1;
+        
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
+        }
+        
+        return newNumber;
+    }
+    
+    /*****
+     * This function should reverse 'num' and return the resulting value.
+     * i.e. if num = 1234
+     *  the function will return 4321
+     *****/
+
+    public double reverseNumber(int num){  // Code swapped in GitDemo-V2
+        double factorialNumber = 0;
+        
+        while( num != 0 ) {
+            double temp = num % 10;
+            factorialNumber = factorialNumber * 10 + temp;
+            num /= 10;
+        }
+        return factorialNumber;
+    }
+    
+}
